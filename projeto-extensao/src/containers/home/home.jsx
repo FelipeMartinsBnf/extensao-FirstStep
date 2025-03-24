a1d5ba04aa234ea3f7eb399fde6de75f99d5fe19
felipemartinsbnf <felipemartinsbn@gmail.com>
2025-03-24 20:46:23 -0300

    Feat: Criando home

---
+++ projeto-extensao/src/containers/home/home.jsx
@@ -0,0 +1,28 @@
+import "./home.css"
+import dev_image from "./../../assets/dev_home.svg"
+
+const Home = () => {
+
+    return (
+        <section className="home_background">
+             <div className="container">
+                <div className="first-step-container">
+                    <div className="content">
+                        <h1>First Step</h1>
+                        <p>Seu primeiro passo na área de tecnologia</p>
+                        <div className="buttons">
+                            <button className="btn">Criar minha conta</button>
+                            <button className="btn">Ver vagas</button>
+                        </div>
+                    </div>
+                    <div className="image">
+                        <img src={dev_image} alt="First Step Illustration" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+
+export default Home
