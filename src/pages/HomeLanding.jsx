aefecd379bb8e47634a87a89c2ecec86910387ac
Felipe Martins <felipemartinsbnf@gmail.com>
2025-05-08 01:56:32 -0300

    fix.

--- src/pages/HomeLanding.jsx
+++ src/pages/HomeLanding.jsx
@@ -1,4 +1,4 @@
-import Header from '../containers/Header'
+import Header from '../containers/Header';
 import Footer from '../containers/Footer';
 import Home from '../containers/Home';
 import "../main.css";
