9d3773491fabaceec7a45caf3a9bedb0f3d17307
Felipe Martins <felipemartinsbn@gmail.com>
2025-05-08 02:09:13 -0300

    fix: arquivo quebrado

---
+++ src/pages/HomeLanding.jsx
@@ -0,0 +1,17 @@
+import Header from '../containers/Header';
+import Footer from '../containers/Footer';
+import Home from '../containers/Home';
+import "../main.css";
+
+
+const HomeLanding = () => {
+    return (
+        <>
+            <Header />
+            <Home></Home>
+            <Footer />
+        </>
+    )
+}
+
+export default HomeLanding
