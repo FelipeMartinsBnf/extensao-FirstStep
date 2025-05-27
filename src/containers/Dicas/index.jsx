5327292a89a817cd81027577ba5315d79b853c6c
felipemartinsbnf <felipemartinsbn@gmail.com>
2025-05-27 20:06:15 -0300

    feat: fazendo quase nada

--- src/containers/Dicas/index.jsx
+++ src/containers/Dicas/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./dicas.css";
+import dropdown from "../../assets/arrow_drop_down_circle.svg"
 
 const Dicas = () => {
 const [entrevista, setEntrevista] = useState([]);
@@ -21,12 +22,16 @@ return (
     <div>
       <h1 className="title-font">Dicas</h1>
       <div>
-        <h2
-          className="title-font-sec sec-title-dicas"
-          onClick={toggleEntrevista}
-        >
-          Dicas para Entrevistas
-        </h2>
+        <div className="dicas-dropdown-btn"  onClick={toggleEntrevista}>
+          <div className="dicas-dropdown-btn-btn">
+            <img src={dropdown} alt="abrir conteudo" className={entrevista ? "dicas-dropdown-icon-open" : "dicas-dropdown-icon-close"} />
+          </div>
+          <h2
+            className="title-font-sec sec-title-dicas"
+          >
+            Dicas para Entrevistas
+          </h2>
+        </div>
         <div
           className={`dicas-content ${
             entrevista ? "dicas-open" : "dicas-close"
@@ -96,12 +101,118 @@ return (
         </div>
       </div>
       <div>
-        <h2
-          className="title-font-sec sec-title-dicas"
-          onClick={toggleHabilidades}
+        <div className="dicas-dropdown-btn"  onClick={toggleHabilidades}>
+          <div className="dicas-dropdown-btn-btn">
+            <img src={dropdown} alt="abrir conteudo" className={habilidades ? "dicas-dropdown-icon-open" : "dicas-dropdown-icon-close"} />
+          </div>
+          <h2
+            className="title-font-sec sec-title-dicas"
+          >
+            Habilidades para Destacar
+          </h2>
+        </div>
+        <div
+          className={`dicas-content ${
+            habilidades ? "dicas-open" : "dicas-close"
+          }`}
         >
-          Habilidades para Destacar
-        </h2>
+          <div className="dicas-item">
+            <div className="dicas-item-header">
+              <h3>1. 💻 Habilidades Técnicas (Depende da Vaga)</h3>
+            </div>
+            <div className="dicas-item-content">
+              <h4 className="dicas-item-title">
+                <span className="dicas-icon">👨‍💻</span> Linguagens de
+                programação:
+              </h4>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Ex.: Python,
+                JavaScript, Java (mesmo em nível básico).
+              </p>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Destaque projetos
+                pessoais ou da faculdade.
+              </p>
+            </div>
+            <div className="dicas-item-content">
+              <h4 className="dicas-item-title">
+                <span className="dicas-icon">🛠️</span>Ferramentas
+              </h4>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Git/GitHub, SQL, Excel
+                (para análise de dados).
+              </p>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Frameworks (React,
+                Node.js, Django – se aplicável).
+              </p>
+            </div>
+            <div className="dicas-item-content">
+              <h4 className="dicas-item-title">
+                <span className="dicas-icon">📖</span>Conceitos importantes
+              </h4>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Lógica de programação,
+                estrutura de dados, algoritmos.
+              </p>
+            </div>
+          </div>
+          <div className="dicas-item">
+            <div className="dicas-item-header">
+              <h3>2. 🌟 Habilidades Comportamentais</h3>
+            </div>
+            <div className="dicas-item-content">
+              <h4 className="dicas-item-title">Proatividade:</h4>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Ex.: "Aprendi
+                [tecnologia] por conta própria para desenvolver [projeto]."
+              </p>
+              <h4 className="dicas-item-title">Trabalho em equipe:</h4>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Ex.: "Participei de um
+                projeto em grupo na faculdade onde [explique sua contribuição]."
+              </p>
+              <h4 className="dicas-item-title">Resolução de problemas:</h4>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Ex.: "Solucionei um
+                erro no código através de pesquisa no Stack Overflow e
+                documentei a solução."
+              </p>
+            </div>
+          </div>
+          <div className="dicas-item">
+            <div className="dicas-item-header">
+              <h3>3. 💬 Comunicação</h3>
+            </div>
+            <div className="dicas-item-content">
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Mantenha{" "}
+                <b>contato visual</b> (se presencial) e comunique-se com
+                clareza.
+              </p>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Evite <b>gírias</b> e
+                mantenha respostas <b>objetivas</b>.
+              </p>
+              <p className="text dicas-item-text">
+                <span className="dicas-icon-sm">🟣</span>Demonstre{" "}
+                <b>entusiasmo</b> e <b>vontade de aprender</b>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="dicas-dropdown-btn"  onClick={toggleHabilidades}>
+          <div className="dicas-dropdown-btn-btn">
+            <img src={dropdown} alt="abrir conteudo" className={habilidades ? "dicas-dropdown-icon-open" : "dicas-dropdown-icon-close"} />
+          </div>
+          <h2
+            className="title-font-sec sec-title-dicas"
+          >
+            Perguntas frequentes
+          </h2>
+        </div>
         <div
           className={`dicas-content ${
             habilidades ? "dicas-open" : "dicas-close"
